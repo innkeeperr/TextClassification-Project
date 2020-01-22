5c57bf5c1543a6b28f920e1d665420904f240b12
innkeeperr <39014566+innkeeperr@users.noreply.github.com>
2020-01-22 12:25:12 +0100

    Wektoryzacja, nowe klasyfikatory, OOM coding

--- AmazonOpinionsProject/Main.py
+++ AmazonOpinionsProject/Main.py
@@ -11,92 +11,202 @@ from nltk.corpus import stopwords
 from textblob import TextBlob
 from textblob import Word
 
-def avg_word(sentence):
-  words = sentence.split()
-  return (sum(len(word) for word in words)/len(words))
 
+# CONFIGURATION #
 display = pd.options.display
 display.width = None
-display.max_columns = 1000
-display.max_rows = 1000
+display.max_columns = 50
+display.max_rows = 50
 display.max_colwidth = 199
 
-df = pd.read_csv('Reviews.csv', nrows=1000, sep=',')
+df = pd.read_csv('Reviews.csv', nrows=10000, sep=',')
 
+# df = pd.DataFrame()
+# temp = pd.read_csv('Reviewstest2.csv', iterator=True, chunksize=1000, sep=',')
+# df = pd.concat(temp, ignore_index=True)
+
+# CONFIGURATION #
+
+# Klasa służąca do opisu DataFrame'u, tworzy nowe kolumny do DF pokazujące różne parametry tesktu
+class TextDescriber:
+  def __init__(self, newDataFrame, text, score):
+    self.newDataFrame = newDataFrame
+    self.text = text
+    self.score = score
+
+  # DataFrame printing
+  def dfPrint(self):
+    print(self.newDataFrame)
+
+  # DataFrame describing
+  def dfDescribe(self):
+    print(self.newDataFrame.describe())
+
+  # wyliczenie ilości słów w opinii
+  def wordCounter (self):
+    self.newDataFrame['word_count'] = self.newDataFrame[self.text].apply(lambda x: len(str(x).split(" ")))
+    print(self.newDataFrame)
+
+  # Wyliczenie ilości znaków w opinii
+  def charCounter (self):
+    self.newDataFrame['char_count'] = self.newDataFrame[self.text].str.len()
+    print(self.newDataFrame)
+
+  # Średnia długość słowa
+  def avgWord(self):
+      self.newDataFrame['avg_word'] = self.newDataFrame[self.text].apply(lambda x: sum(len(x) for x in x.split())/len(x.split()))
+      print(self.newDataFrame)
+
+  # Zapisanie stop words w osobnej kolumnie (liczba stopwords)
+  def stopwordsCounter(self):
+    stop = stopwords.words('english')
+    self.newDataFrame['stopwords'] = self.newDataFrame[self.text].apply(lambda x: len([x for x in x.split() if x in stop]))
+
+  # Ile liczb w tekście (do usuniecia)
+  def numericCounter(self):
+    self.newDataFrame['numerics'] = self.newDataFrame[self.text].apply(lambda x: len([x for x in x.split() if x.isdigit()]))
+
+
+
+# Główny DataFrame, który zostanie wykorzystany w trenowaniu modelu
 trainDF = pd.DataFrame()
 trainDF["Rating"] = df["Score"]
 trainDF["Opinion"] = df["Text"]
 
-# print(trainDF)
-# print(trainDF.describe())
-
-# # Utworzenie nowego DataFrame
+# Utworzenie nowego DataFrame, który posłuży do rozeznania się w tekście
 # train = trainDF.copy()
 #
-# # wyliczenie ilości słów w opinii
-# train['word_count'] = train['Opinion'].apply(lambda x: len(str(x).split(" ")))
-# # train[['Opinion', 'word_count']].head()
-#
-# # Wyliczenie ilości znaków w opinii
-# train['char_count'] = train['Opinion'].str.len()
-# # train[['Opinion','char_count']].head()
-#
-# # Średnia długość słowa
-# train['avg_word'] = train['Opinion'].apply(lambda x: avg_word(x))
-# # train[['Opinion','avg_word']].head()
-#
-# # Zapisanie stop words w osobnej kolumnie (liczba stopwords)
-# stop = stopwords.words('english')
-# train['stopwords'] = train['Opinion'].apply(lambda x: len([x for x in x.split() if x in stop]))
-# # train[['Opinion', 'stopwords']].head()
-#
-# # Ile liczb w tekście (do usuniecia)
-# train['numerics'] = train['Opinion'].apply(lambda x: len([x for x in x.split() if x.isdigit()]))
-# # train[['Opinion', 'numerics']].head()
-# print(train)
+# TextAnalyzer = TextDescriber(train, "Opinion", "Rating" )
+# TextAnalyzer.wordCounter()
+# TextAnalyzer.avgWord()
 
 
+#################################
 # OCZYSZCZANIE TEKSTU
-# Zmiana liter na małe
-trainDF['Opinion'] = trainDF['Opinion'].apply(lambda x: " ".join(x.lower() for x in x.split()))
 
-# usuniecie znaków interpunkcyjnych
-trainDF['Opinion'] = trainDF['Opinion'].str.replace('[^\w\s]','')
+# Klasa pomagająca oczyścić tekst znajdujący się w kolumnie DataFrame
+class DataFrameCleaner:
+  def __init__(self, mainDF, text, score):
+    self.mainDF = mainDF
+    self.text = text
+    self.score = score
 
-#Usuniecie z opinii "stop words"
-stop = stopwords.words('english')
-trainDF['Opinion'] = trainDF['Opinion'].apply(lambda x: " ".join(x for x in x.split() if x not in stop))
+  # DataFrame printing
+  def dfPrint(self):
+    print(self.mainDF)
 
-# Ktore slowa powtarzaja sie najczesciej + usuniecie ich
-freq = pd.Series(' '.join(trainDF['Opinion']).split()).value_counts()[:10]
-# print(freq)
-freq = list(freq.index)
-trainDF['Opinion'] = trainDF['Opinion'].apply(lambda x: " ".join(x for x in x.split() if x not in freq))
+  # Zmiana liter na małe
+  def dfLowerCase(self):
+    self.mainDF[self.text] = self.mainDF[self.text].apply(lambda x: " ".join(x.lower() for x in x.split()))
+    return(self.mainDF)
 
-# Ktore slowa powtarzaja sie najrzadziej + usuniecie ich
-freqMin = pd.Series(' '.join(trainDF['Opinion']).split()).value_counts()[-10:]
-# print(freqMin)
-freqMin = list(freqMin.index)
-trainDF['Opinion'] = trainDF['Opinion'].apply(lambda x: " ".join(x for x in x.split() if x not in freqMin))
+  # Usuniecie znaków interpunkcyjnych
+  def dfMarksRemove(self):
+    self.mainDF[self.text] = self.mainDF[self.text].str.replace('[^\w\s]', '')
+    return self.mainDF
 
-# Poprawienie słów, ktore zostały napisane z błędem (funkcja correct() wykonuje się bardzo długo)
-# trainDF['Opinion'].apply(lambda x: str(TextBlob(x).correct()))
+  # Usuniecie z opinii "stop words"
+  def dfStopwordsRemove(self):
+    stop = stopwords.words('english')
+    self.mainDF[self.text] = self.mainDF[self.text].apply(lambda x: " ".join(x for x in x.split() if x not in stop))
+    return self.mainDF
 
-# Lematyzacja tekstu (jedynie trzon słów), lematyzacja > stemming
-trainDF['Opinion'] = trainDF['Opinion'].apply(lambda x: " ".join([Word(word).lemmatize() for word in x.split()]))
+  # Usunięcie słów, które powtarzają sie najczęściej
+  def dfMostFreqWords(self):
+    freq = pd.Series(' '.join(self.mainDF[self.text]).split()).value_counts()[:10]
+    freq = list(freq.index)
+    self.mainDF[self.text] = self.mainDF[self.text].apply(lambda x: " ".join(x for x in x.split() if x not in freq))
+    return self.mainDF
 
-# print(trainDF)
-####################################
+  # Usunięcie słów, które powtarzają sie najrzadziej
+  def dfLeastOftenWords(self):
+    freqMin = pd.Series(' '.join(self.mainDF[self.text]).split()).value_counts()[-10:]
+    freqMin = list(freqMin.index)
+    self.mainDF[self.text] = self.mainDF[self.text].apply(lambda x: " ".join(x for x in x.split() if x not in freqMin))
+    return self.mainDF
 
-#split dataset and encode labels
-# split the dataset into training and validation datasets
-train_x, valid_x, train_y, valid_y = model_selection.train_test_split(trainDF['Opinion'], trainDF['Score'])
+  # Poprawienie słów, ktore zostały napisane z błędem (funkcja correct() wykonuje się bardzo długo)
+  def dfCorrectWords(self):
+    self.mainDF[self.text].apply(lambda x: str(TextBlob(x).correct()))
+    return self.mainDF
 
-# label encode the target variable
+  # Lematyzacja tekstu (jedynie trzon słów), lematyzacja > stemming
+  def dfLemmatize(self):
+    self.mainDF[self.text] = self.mainDF[self.text].apply(lambda x: " ".join([Word(word).lemmatize() for word in x.split()]))
+    return self.mainDF
+
+# Użycie klasy do oczyszczenia tekstu
+mainTrainDF = DataFrameCleaner(trainDF, 'Opinion', 'Rating')
+# Pomocniczy obiekt to obliczenia, ile słów zostało usuniętych
+TextAnalyzer = TextDescriber(trainDF, "Opinion", "Rating" )
+# Przed oczyszczaniem tekstu (print)
+# TextAnalyzer.wordCounter()
+# Metody
+trainDF = mainTrainDF.dfLowerCase()
+trainDF = mainTrainDF.dfMarksRemove()
+trainDF = mainTrainDF.dfStopwordsRemove()
+trainDF = mainTrainDF.dfMostFreqWords()
+trainDF = mainTrainDF.dfLeastOftenWords()
+trainDF = mainTrainDF.dfLemmatize()
+print(trainDF)
+# Po oczyszczeniu (print)
+# TextAnalyzer.wordCounter()
+
+# END OF TEXT CLEARING #
+
+#####################################
+
+# MODEL TRAINING #
+# Podział na zestaw treningowy i walidacyjny
+train_x, valid_x, train_y, valid_y = model_selection.train_test_split(trainDF['Opinion'], trainDF['Rating'])
 encoder = preprocessing.LabelEncoder()
 train_y = encoder.fit_transform(train_y)
 valid_y = encoder.fit_transform(valid_y)
 
-print(valid_y)
+# WEKTORYZACJA TEKSTU
+# Term frequency & inverse document frequency (word level tf-idf)
+tfidf_vect = TfidfVectorizer(analyzer='word', token_pattern=r'\w{1,}', max_features=5000)
+tfidf_vect.fit(trainDF['Opinion'])
+xtrain_tfidf =  tfidf_vect.transform(train_x)
+xvalid_tfidf =  tfidf_vect.transform(valid_x)
 
+# N-Gram level tf-idf
+tfidf_vect_ngram = TfidfVectorizer(analyzer='word', token_pattern=r'\w{1,}', ngram_range = (2, 3), max_features=5000)
+tfidf_vect_ngram.fit(trainDF['Opinion'])
+xtrain_tfidf_ngram =  tfidf_vect_ngram.transform(train_x)
+xvalid_tfidf_ngram =  tfidf_vect_ngram.transform(valid_x)
 
+#################
+
+def train_model(classifier, feature_vector_train, label, feature_vector_valid, is_neural_net=False):
+  # fit the training dataset on the classifier
+  classifier.fit(feature_vector_train, label)
+
+  # predict the labels on validation dataset
+  predictions = classifier.predict(feature_vector_valid)
+
+  if is_neural_net:
+    predictions = predictions.argmax(axis=-1)
+
+  return metrics.accuracy_score(predictions, valid_y)
+
+# Sprawdzeniue różnych klasyfikatorów
+# Linear Classifier on Word Level TF IDF Vectors
+classifier = linear_model.LogisticRegression()
+accuracy = train_model(classifier, xtrain_tfidf, train_y, xvalid_tfidf)
+print ("LR, WordLevel TF-IDF: ", accuracy)
+
+# Naive Bayes on Word Level TF IDF Vectors
+classifier = naive_bayes.MultinomialNB()
+accuracy = train_model(classifier, xtrain_tfidf, train_y, xvalid_tfidf)
+print ("NB, WordLevel TF-IDF: ", accuracy)
+
+# SVM on Ngram Level TF IDF Vectors
+classifier = svm.SVC()
+accuracy = train_model(classifier, xtrain_tfidf_ngram, train_y, xvalid_tfidf_ngram)
+print("SVM N-Gram Vectors: ", accuracy)
+
+# RF on Word Level TF IDF Vectors
+classifier = ensemble.RandomForestClassifier()
+accuracy = train_model(classifier, xtrain_tfidf, train_y, xvalid_tfidf)
+print ("RF, WordLevel TF-IDF: ", accuracy)
